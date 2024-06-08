73ea6e7bc2a21d872aa93cf8562eaa66bd3c968d
SuwasG <suwasghale693@gmail.com>
2024-06-08 13:14:20 +0545

    initial boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/*.tsx',
+    './src/utils/*.ts',
+    './src/components/*.tsx',
+    './src/components/ui/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
